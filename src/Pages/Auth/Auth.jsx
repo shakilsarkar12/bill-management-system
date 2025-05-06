4bbff5fbe5a6f6bb1b648d0f41becf7532ccf0f1
shakil <shakil.sarkar1932@gmail.com>
2025-05-07 01:08:19 +0600

    firebase setup

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Outlet } from 'react-router';
 
 const Auth = () => {
     return (
         <div>
-            auth
+            <Outlet/>
         </div>
     );
 };
